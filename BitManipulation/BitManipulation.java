4db69e72fa557fefe4cead3f56b27d8440cc5157
GURURAJ115 <gururajsnaik115@gmail.com>
2024-07-31 19:45:50 +0530

    Clearing n bits

--- BitManipulation/BitManipulation.java
+++ BitManipulation/BitManipulation.java
@@ -25,16 +25,22 @@ public class BitManipulation {
         int bitMask = ~(1<<i);
         return n&bitMask;
     }
+    public static int clearIBits(int n, int i){
+        int bitMask = (~0)<<i;
+        return n&bitMask;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number :");
         int n = sc.nextInt();
         System.out.println("Enter the position :");
         int i = sc.nextInt();
+        sc.close();
         System.out.print("Given Number is : ");
         oddOrEven(n);
         System.out.println("The "+i+" bit is : "+getIthBit(n, i));
         System.out.println("After setting the "+i+" bit: "+setIthBit(n, i));
         System.out.println("After clearing the "+i+" bit: "+clearIthBit(n, i));
+        System.out.println("After clearing "+i+" bits:"+clearIBits(n,i));
     }
 }
