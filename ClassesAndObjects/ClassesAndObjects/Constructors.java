f272e45031ae88fd6e1472950aa476542ae701a6
GURURAJ115 <gururajsnaik115@gmail.com>
2024-08-02 16:00:36 +0530

    Constructor Overloading , Parmeterized and non- Parameterized

--- ClassesAndObjects/ClassesAndObjects/Constructors.java
+++ ClassesAndObjects/ClassesAndObjects/Constructors.java
@@ -2,7 +2,9 @@ package ClassesAndObjects;
 public class Constructors {
     public static void main(String[] args) {
         // Student s1 = new Student("Gururaj");
-        Student s1 = new Student();
+        Student s0 = new Student();
+        Student s1 = new Student("Guru");
+        Student s2 = new Student(12);
         // System.out.println(s1.name);
     }
 }
@@ -11,10 +13,19 @@ class Student{
     String name;
     int roll;
 //default constructor will be called by  java if no constructors are defined by the user.
-    // Student(String name){
-    Student(){
-    //This is a constructor
-    // this.name = name;
-    System.out.println("Constructor is called....");
+    
+    Student(String name){
+    // This is a constructor
+    // Parameterized Constructor
+    this.name = name;
     }
+    Student(int roll){
+        this.roll = roll;
+    }
+
+    Student(){
+    System.out.println("Constructor is called....");
+    //This is non parameterized constructor
+    }
+    
 }
