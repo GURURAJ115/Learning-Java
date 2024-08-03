03ef26c15c6bb3c0d9b3ef3fdbca556f7d73c62f
GURURAJ115 <gururajsnaik115@gmail.com>
2024-08-03 16:50:52 +0530

    Function Overloading and Overriding

---
+++ ClassesAndObjects/FunOverloadingandOverriding.java
@@ -0,0 +1,18 @@
+public class FunOverloadingandOverriding{
+    public static void main(String[] args) {
+        
+    }
+}
+class Employee{
+    public void something(){
+        System.out.println("Hello");
+    }//Function overloading
+    public void something(int x){
+        System.out.println("OHO");
+    }
+}
+class Peon extends Employee{
+    public void something(){
+        System.out.println("OHnooo");
+    }//Function overriding
+}
