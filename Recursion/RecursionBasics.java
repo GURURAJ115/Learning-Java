7eb6b7f5c60ff17234b88684f6ed01e566a2eeaa
GURURAJ115 <gururajsnaik115@gmail.com>
2024-08-05 16:59:49 +0530

    IsSorted , First occurence , power of a number

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -40,6 +40,33 @@ public class RecursionBasics {
         if(n==1||n==0) return n;
         return fib(n-1)+fib(n-2);
     }
+
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]) {
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
+
+    public static int firstOccurance(int arr[],int key,int i){
+        if(arr[i]==key) {
+            return i;
+        }
+        if(i==arr.length-1){
+            return -1;
+        }
+        return firstOccurance(arr, key, i+1);
+    }
+
+    public static int power(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        return x*power(x, n-1);
+    }
     public static void main(String[] args) {
         int n = 6;
         System.out.println("Decreasing");
@@ -49,6 +76,10 @@ public class RecursionBasics {
         System.out.println(fact(n));
         System.out.println(Sumofn(n));
         System.out.println(fib(n));
+        int arr[] = {1,2,3,4,5};
+        System.out.println(isSorted(arr, 0));
+        System.out.println(firstOccurance(arr, 6, 0));
+        System.out.println(power(2, 3));
     }
 }
 
