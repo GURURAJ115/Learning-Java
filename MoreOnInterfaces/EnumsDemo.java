b52b9f5817cd07e2af9cee551a0ef8cf4bfb3299
GURURAJ115 <gururajsnaik115@gmail.com>
2024-08-31 21:46:47 +0530

    If and switch with enum

--- MoreOnInterfaces/EnumsDemo.java
+++ MoreOnInterfaces/EnumsDemo.java
@@ -3,11 +3,41 @@ package MoreOnInterfaces;
 enum Status{
     Running,Failed,Pending,Success;
 }
+// enum extends enum class java.lang.enum;
+
 public class EnumsDemo {
     public static void main(String[] args) {
-        Status[] ss = Status.values();
-        for(Status s:ss){
-            System.out.println(s+":"+s.ordinal());
+        // Status[] ss = Status.values();
+        // for(Status s:ss){
+        //     System.out.println(s+":"+s.ordinal());
+        // }
+        Status s = Status.Pending;
+
+        switch (s) {
+            case Running:
+                System.out.println("All Good");
+                break;
+            
+            case Failed:
+                System.out.println("Try Again");
+                break;
+            
+            case Pending:
+                System.out.println("Please Wait");
+                break;
+            
+            default:
+                System.out.println("Done");
+                break;
         }
+        s=Status.Success;
+        if(s==Status.Running)
+            System.out.println("All Good");
+        else if(s==Status.Failed)
+            System.out.println("Try Again");
+        else if(s==Status.Pending)
+            System.out.println("Please Wait");
+        else
+            System.out.println("Done");
     }
 }
