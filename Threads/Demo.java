f053fd0e2662ebe5fb73b0a43aebe9ca06fcc102
GURURAJ115 <gururajsnaik115@gmail.com>
2024-09-28 11:47:43 +0530

    Normal Thread implementation

---
+++ Threads/Demo.java
@@ -0,0 +1,26 @@
+package Threads;
+
+class A extends Thread{
+    public void run(){
+        for(int i=1;i<=100;i++){
+            System.out.println("hi");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i=1;i<=100;i++){
+            System.out.println("hello");
+        }
+    }
+}
+public class Demo {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+        System.out.println(obj1.getPriority());
+        obj1.start();
+        obj2.start();
+    }
+}
