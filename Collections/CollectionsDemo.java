ea8d554d09e9229887135481847bccaa57db16e4
GURURAJ115 <gururajsnaik115@gmail.com>
2024-10-14 11:54:58 +0530

    Use of comparable

--- Collections/CollectionsDemo.java
+++ Collections/CollectionsDemo.java
@@ -5,31 +5,65 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+class Student 
+implements Comparable<Student>
+{
+    int age;
+    String name;
+    
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+
+    @Override
+    public int compareTo(Student that) {
+        if(this.age>that.age){
+            return 1;
+        }
+        else 
+            return -1;
+    }
+    
+}
 public class CollectionsDemo {
     public static void main(String[] args) {
         // To get this done we use comparator
-        Comparator<Integer> com = new Comparator<Integer>()
-        {
-            public int compare(Integer i,Integer j){
-                if(i%10>j%10)
-                    return 1;
-                else 
-                    return -1;
-            }
-        };
+        // Comparator<Student> com = new Comparator<Student>()
+        // {
+        //     public int compare(Student i,Student j){
+        //         if(i.age>j.age)
+        //             return 1;
+        //         else 
+        //             return -1;
+        //     }
+        // };
 
-        List<Integer> nums = new ArrayList<>();
+        Comparator<Student> com = (i,j) -> i.age>j.age?1:-1;
+
+        List<Student> studs = new ArrayList<>();
         //To sort based on our own logic like based on the last digit
-        nums.add(43);
-        nums.add(31);
-        nums.add(72);
-        nums.add(29);
+        studs.add(new Student(21, "Navin"));
+        studs.add(new Student(12, "John"));
+        studs.add(new Student(18, "Parul"));
+        studs.add(new Student(20, "Kiran"));
+        for(Student s : studs){
+            System.out.println(s);
+        }
+        System.out.println();
         //normal sort 29 31 43 72
-        Collections.sort(nums);
-        System.out.println(nums);
+        // Collections.sort(studs);
+        // System.out.println(studs);
         //using comparator 31 72 43 29
-        Collections.sort(nums,com);
-        System.out.println(nums);
+        // Collections.sort(studs,com);
+        Collections.sort(studs);
+        // System.out.println(studs);
+        for(Student s : studs){
+            System.out.println(s);
+        }
     }
 }
